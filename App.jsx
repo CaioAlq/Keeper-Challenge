f7a393763a6f3a35a47f5ec44bb4b29348e00186
Caio <caio@pop-os.localdomain>
2023-04-22 01:01:10 -0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+import "./src/reset.css"
+
+import Header from "./src/components/Header/Header";
+import Footer from "./src/components/Footer/Footer";
+import Note from "./src/components/Note/Note";
+import CreateArea from "./src/components/CreateArea/CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
